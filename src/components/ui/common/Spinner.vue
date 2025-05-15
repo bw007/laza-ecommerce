10d75803c3248d2648c261090a2b1811a0ed4a87
bw007 <mr.abdulaziz00791@gmail.com>
2025-05-15 17:39:28 +0500

    feat: add Spinner component

---
+++ src/components/ui/common/Spinner.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    role="status"
+    class="inline-block animate-spin rounded-full border-4 border-t-transparent border-blue-600 h-8 w-8"
+  >
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
