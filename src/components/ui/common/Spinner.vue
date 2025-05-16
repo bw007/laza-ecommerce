6a73ad3553fabbaf279d05e09650daba37f8bbf1
bw007 <mr.abdulaziz00791@gmail.com>
2025-05-16 15:50:07 +0500

    fix: update Spinner component border color to use primary variable

--- src/components/ui/common/Spinner.vue
+++ src/components/ui/common/Spinner.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     role="status"
-    class="inline-block animate-spin rounded-full border-4 border-t-transparent border-blue-600 h-8 w-8"
+    class="inline-block animate-spin rounded-full border-4 border-t-transparent border-primary h-8 w-8"
   >
     <span class="sr-only">Loading...</span>
   </div>
