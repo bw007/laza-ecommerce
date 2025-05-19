6a3e477888754a34a0c8b76a8f7679de355fe9df
bw007 <abdulaziz00791@gmail.com>
2025-05-19 07:20:57 +0500

    feat: add ContentLoader component with Spinner

---
+++ src/components/common/ContentLoader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Spinner from './Spinner.vue';
+
+</script>
+
+<template>
+  <div class="w-full h-full flex items-center justify-center">
+    <Spinner />
+  </div>
+</template>
