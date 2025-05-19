c51c69507f6d9ae9f8b4b7ba699e1e320cfa8add
bw007 <abdulaziz00791@gmail.com>
2025-05-19 07:21:28 +0500

    refactor: remove AppLoader and Spinner components

--- src/components/ui/common/Spinner.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div
-    role="status"
-    class="inline-block animate-spin rounded-full border-4 border-t-transparent border-primary h-8 w-8"
-  >
-    <span class="sr-only">Loading...</span>
-  </div>
-</template>
