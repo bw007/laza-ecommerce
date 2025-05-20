0e6d846463bf7db8aeb2011a489c4e69f0f177d9
bw007 <mr.abdulaziz00791@gmail.com>
2025-05-20 17:44:34 +0500

    feat: integrate TheSidebar component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import BottomNav from '@/components/layout/TheBottomNav.vue';
 import TheHeader from '@/components/layout/TheHeader.vue';
+import TheSidebar from '@/components/layout/TheSidebar.vue';
 
 </script>
 
 <template>
   <div class="flex flex-col h-screen">
     <TheHeader />
+    <TheSidebar />
     <div class="flex-1">
       <RouterView />
     </div>
-    <BottomNav />
+    <BottomNav class="sm:hidden" />
   </div>
 </template>
