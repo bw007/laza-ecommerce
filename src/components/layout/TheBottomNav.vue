69f62135d96220c4d6992d406959a87e65b70370
bw007 <mr.abdulaziz00791@gmail.com>
2025-05-20 17:44:56 +0500

    feat: enhance TheBottomNav component with computed

--- src/components/layout/TheBottomNav.vue
+++ src/components/layout/TheBottomNav.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { Heart, House, ShoppingCart, Tags } from 'lucide-vue-next';
 import Tabs from '../ui/tabs/Tabs.vue';
 import TabsList from '../ui/tabs/TabsList.vue';
@@ -10,7 +10,7 @@ import mainRoutes from '@/router/routes/main';
 const router = useRouter();
 const route = useRoute();
 
-const activeTab = ref(route.path);
+const activeTab = ref<string>(route.path)
 
 watch(() => route.path, (newPath) => {
   activeTab.value = newPath;
@@ -33,6 +33,10 @@ const getIconComponent = (iconName: string) => {
   return iconMap[iconName as keyof typeof iconMap] || House;
 };
 
+const bottomNavRoutes = computed(() =>
+  mainRoutes.filter(route => route.meta?.bottomNav)
+);
+
 </script>
 
 <template>
@@ -44,18 +48,19 @@ const getIconComponent = (iconName: string) => {
   >
     <TabsList class="flex justify-between w-full h-full bg-white">
       <TabsTrigger
-        v-for="nav in mainRoutes"
-        :key="nav.path"
-        :value="nav.path"
+        v-for="navitem in bottomNavRoutes"
+        :key="navitem.path"
+        :value="navitem.path"
         class="rounded-none flex flex-col items-center justify-center"
+        :class="[activeTab === navitem.path ? '!bg-purple-50' : '']"
       >
         <component
-          v-if="activeTab !== nav.path"
-          :is="getIconComponent(nav.meta.icon)"
+          v-if="activeTab !== navitem.path"
+          :is="getIconComponent(navitem.meta.icon)"
           :size="18"
           class="text-muted-foreground"
         />
-        <span v-else class="text-[11px] text-primary">{{ nav.name }}</span>
+        <span v-else class="text-[11px] text-primary">{{ navitem.name }}</span>
       </TabsTrigger>
     </TabsList>
   </Tabs>
