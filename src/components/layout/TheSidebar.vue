56e6a48b6f6d553ee24e076a20fe132688a19732
bw007 <mr.abdulaziz00791@gmail.com>
2025-05-20 17:45:12 +0500

    feat: add TheSidebar component

---
+++ src/components/layout/TheSidebar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '../ui/sheet';
+
+</script>
+
+<template>
+  <Sheet>
+    <SheetTrigger class="w-12 h-12 bg-background rounded-full flex items-center justify-center active:opacity-65">
+      <img src="@/assets/images/bar-icon.svg" alt="Menu">
+    </SheetTrigger>
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Are you absolutely sure?</SheetTitle>
+        <SheetDescription>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </SheetDescription>
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
+</template>
