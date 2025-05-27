6d4e8622a96011b98c4c3325e44d70abc08bcfa3
bw007 <mr.abdulaziz00791@gmail.com>
2025-05-27 17:14:58 +0500

    feat: enhance MainLayout with sidebar and filter functionality

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,11 +2,26 @@
 import TheBottomNav from '@/components/layout/TheBottomNav.vue';
 import TheHeader from '@/components/layout/TheHeader.vue';
 
+const openSidebar = () => {
+console.log(100);
+
+}
+
+const openFilter = () => {
+console.log(200);
+
+}
+
 </script>
 
 <template>
   <div class="flex flex-col">
-    <TheHeader />
+    <TheHeader
+      :show-menu="true"
+      :show-filter="true"
+      @menu-click="openSidebar"
+      @filter-click="openFilter"
+    />
     <div class="flex-1 h-full mb-20">
       <RouterView />
     </div>
