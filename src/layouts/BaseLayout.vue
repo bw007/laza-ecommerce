61b59898c137b016fd95fe490abc5c6e2008efb9
bw007 <mr.khasanov02@gmail.com>
2025-05-27 07:41:50 +0500

    feat: create BaseLayout component for dynamic layout rendering

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import MainLayout from './MainLayout.vue';
+
+const route = useRoute()
+const currentLayout = computed(() => {
+  return route.meta?.layout || 'main'
+})
+
+const layoutComponents = {
+  'main': MainLayout,
+}
+
+</script>
+
+<template>
+  <component :is="layoutComponents[currentLayout as keyof typeof layoutComponents]">
+    <RouterView />
+  </component>
+</template>
