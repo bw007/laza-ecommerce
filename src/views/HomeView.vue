e07f3958dea01710983586c578b9fef1518185a2
bw007 <mr.khasanov02@gmail.com>
2025-05-30 07:54:27 +0500

    feat: update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import SearchBar from '@/components/search/SearchBar.vue';
+
 
 </script>
 
 <template>
-
+  <div class="space-y-1">
+    <h2 class="text-[28px] font-semibold leading-[110%]">Hello</h2>
+    <p class="text-muted-foreground text-[15px] ">Welcome to Laza.</p>
+  </div>
+  <SearchBar class="w-full" />
 </template>
