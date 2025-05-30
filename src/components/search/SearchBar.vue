7f1ab0b9551e8d0434cfe44f1d238bb42bc44c68
bw007 <mr.khasanov02@gmail.com>
2025-05-30 07:54:16 +0500

    feat: add SearchBar

---
+++ src/components/search/SearchBar.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { Check, Mic, Search } from 'lucide-vue-next'
+import { cn } from '@/lib/utils'
+import { Combobox, ComboboxAnchor, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxItemIndicator, ComboboxList } from '@/components/ui/combobox'
+import Button from '../ui/button/Button.vue'
+
+const frameworks = [
+  { value: 'next.js', label: 'Next.js' },
+  { value: 'sveltekit', label: 'SvelteKit' },
+  { value: 'nuxt', label: 'Nuxt' },
+  { value: 'remix', label: 'Remix' },
+  { value: 'astro', label: 'Astro' },
+]
+</script>
+
+<template>
+  <div class="flex w-full justify-between items-center">
+    <Combobox by="label">
+      <ComboboxAnchor>
+        <div class="relative w-full max-w-sm items-center ">
+          <ComboboxInput class="pl-9" :display-value="(val) => val?.label ?? ''" placeholder="Select framework..." />
+          <span class="absolute start-0 inset-y-0 flex items-center justify-center px-3">
+            <Search class="size-4 text-muted-foreground" />
+          </span>
+        </div>
+      </ComboboxAnchor>
+
+      <ComboboxList>
+        <ComboboxEmpty>
+          No framework found.
+        </ComboboxEmpty>
+
+        <ComboboxGroup>
+          <ComboboxItem
+            v-for="framework in frameworks"
+            :key="framework.value"
+            :value="framework"
+          >
+            {{ framework.label }}
+
+            <ComboboxItemIndicator>
+              <Check :class="cn('ml-auto h-4 w-4')" />
+            </ComboboxItemIndicator>
+          </ComboboxItem>
+        </ComboboxGroup>
+      </ComboboxList>
+    </Combobox>
+    <Button>
+      <Mic />
+    </Button>
+  </div>
+</template>
