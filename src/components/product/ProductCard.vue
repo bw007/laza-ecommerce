0a1df86ddf3d8b46d75e9612a7266b24d1a12f4c
bw007 <mr.khasanov02@gmail.com>
2025-06-03 08:26:30 +0500

    feat: enhance ProductCard component with Card

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import type { Product } from '@/types/product';
 import { ref } from 'vue';
+import Card from '../ui/card/Card.vue';
+import CardContent from '../ui/card/CardContent.vue';
+import { Heart } from 'lucide-vue-next';
+import Button from '../ui/button/Button.vue';
 
 const props = defineProps<{
   product: Product;
@@ -10,9 +14,15 @@ const product = ref(props.product);
 </script>
 
 <template>
-  <div class="border border-gray-200 rounded-lg p-4">
-    <img :src="product.image" :alt="product.name" class="w-full h-48 object-cover rounded-lg mb-4">
-    <h3 class="text-lg font-semibold">{{ product.name }}</h3>
-    <p class="text-gray-600">{{ product.price }}</p>
-  </div>
+  <Card class="border-none shadow-none bg-transparent">
+    <CardContent class="p-0">
+      <div class="w-full bg-background relative rounded-[15px]">
+        <Button variant="ghost" size="icon" class="absolute top-3 right-2.5">
+          <Heart class="size-6 text-muted-foreground" />
+        </Button>
+        <img :src="product.image" :alt="product.name" class="object-cover">
+      </div>
+      
+    </CardContent>
+  </Card>
 </template>
