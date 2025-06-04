7d8b0eda0d7168e2ab08bb6e37438e92ead7bc70
bw007 <mr.abdulaziz00791@gmail.com>
2025-06-04 17:58:46 +0500

    feat: create ProductsGridLayout component

---
+++ src/layouts/ProductsGridLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import TheHeader from '@/components/layout/TheHeader.vue';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  console.log(100);
+  
+})
+
+const backHandler = () => {
+  console.log("Back");
+}
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <TheHeader
+      :show-back="true"
+      @back-click="backHandler"
+    />
+    <div class="mb-20 px-5">
+      <RouterView />
+    </div>
+  </div>
+</template>
